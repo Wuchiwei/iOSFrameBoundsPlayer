3cf3d388bd76c17dc129578d6bda67b975aaca79
Luke <wuchiwei17@gmail.com>
2019-07-20 01:29:34 +0800

    Establish View.

--- iOSFrameBoundsPlayer/ViewController.swift
+++ iOSFrameBoundsPlayer/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var xSlider: UISlider!
+    @IBOutlet weak var ySlider: UISlider!
+    @IBOutlet weak var widthSlider: UISlider!
+    @IBOutlet weak var heightSlider: UISlider!
+    
+    @IBOutlet weak var viewSeletor: UISegmentedControl!
+    @IBOutlet weak var frameBoundsSeletor: UISegmentedControl!
+    
+    @IBOutlet weak var ntuSuperView: UIView!
+    @IBOutlet weak var ntuChildView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
     }
 
 
